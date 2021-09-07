b8309816ea04bca2fe69b055ed4f0be22289e437
ngle-kieu <61229802+ngle-kieu@users.noreply.github.com>
2021-09-07 13:33:48 +0200

    Add show graph.

--- dash_sample.py
+++ dash_sample.py
@@ -1,26 +1,31 @@
 import dash
-import dash_core_components as dcc
-from dash_core_components.ConfirmDialog import ConfirmDialog
-from dash_core_components.ConfirmDialogProvider import ConfirmDialogProvider
-import dash_html_components as html
+from dash import dcc
+from dash import html
+import plotly.graph_objects as go
+
+fig = go.Figure(
+    data=[go.Bar(y=[2, 1, 3])],
+    layout_title_text="A Sample Graph"
+)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout= html.Div(
+app.config.suppress_callback_exceptions = True
+app.layout = html.Div(
     children=[
         html.H1(children='Hello Dash'),
         dcc.Dropdown(
             id='greeting_dropdown',
             options=[
-                {'label':'English', 'value':'Hello'},
-                {'label':'German', 'value':'Hallo'},
-                {'label':'Japanese', 'value':'Konichiwa'},
-                {'label':'Vietnamese', 'value':'Chao'}
+                {'label': 'English', 'value': 'Hello'},
+                {'label': 'German', 'value': 'Hallo'},
+                {'label': 'Japanese', 'value': 'Konichiwa'},
+                {'label': 'Vietnamese', 'value': 'Chao'}
             ],
-            value='Hey',
+            value='Greeting',
         ),
+        html.Div(id='output_dropdown'),
         dcc.ConfirmDialogProvider(
             children=html.Button(
                 'Show graph'
@@ -28,26 +33,31 @@ app.layout= html.Div(
             id='confirm_graph',
             message='Are you sure you want to view the graph?',
         ),
-        html.Div(id='output_dropdown'),
-        html.Div(id='output_dialog_provider'),
+        html.Div(id='output_graph',
+                 children=dcc.Graph(figure=fig),
+                 style={'display': 'block'})
     ]
 )
+
 
 @app.callback(
     dash.dependencies.Output('output_dropdown', 'children'),
     dash.dependencies.Input('greeting_dropdown', 'value'))
 def update_output_dropdown(value):
+    if value is None:
+        value = 'Greeting'
     return '{} Dash.'.format(value)
 
+
 @app.callback(
-    dash.dependencies.Output('output_dialog_provider', 'children'),
+    dash.dependencies.Output('output_graph', 'style'),
     dash.dependencies.Input('confirm_graph', 'submit_n_clicks'))
 def update_output_dropdown(submit_n_clicks):
     if not submit_n_clicks:
-        return ''
-    return """
-        Graph shown!
-    """
+        return {'display': 'none'}
+    else:
+        return {'display': 'block'}
 
-if __name__=='__main__':
-    app.run_server(debug=True,port=8050)
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8052)
